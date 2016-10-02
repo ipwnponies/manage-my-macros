6ae8089958ced36975074301d7c081cb3cd3ae9f
John Nguyen <jngu@yelp.com>
2016-10-02 00:36:03 -0700

    Set up boilerplate to start/stop flask server

---
+++ src/OAuth2ResponseServer.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+# Configure the listening server and port
+app.config['SERVER_NAME']='127.0.0.1:13337'
+
+class OAuth2ResponseServer:
+    def __init__(self):
+        pass
+
+    @app.route('/optparam/')
+    @app.route('/optparam/<foo>')
+    def optional_parameter(foo=None):
+        return render_template('hello.html', bar=foo)
+
+    @app.route('/caseSensitive/')
+    def case_sensitive():
+        return 'The url is case sensitive'
+
+    @app.route('/type/<int:bar>')
+    def type_check(bar):
+        return 'bar {}'.format(bar)
+
+    @app.route('/')
+    def index():
+        return 'hi'
+
+    @app.route('/bye/')
+    def bye():
+        Server.shutdown()
+        return 'bye! shutting down'
+
+    def start(self):
+        app.run()
+
+    def shutdown(self):
+        func = request.environ.get('werkzeug.server.shutdown')
+        if func:
+            raise RuntimeError('Not running with the Werkzeug Server')
+        else:
+            func()
+
+if __name__ == '__main__':
+    responseServer = OAuth2ResponseServer()
+    responseServer.start()
+    responseServer.shutdown()
